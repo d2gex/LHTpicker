ad8dcc872d7223d0d05ab30121a1076363ad7858
d2gex <daniel.garcia@d2garcia.com>
2024-03-19 18:16:03 +0100

    Rename testing sample data

--- tests/testthat/helper-taxon_extractor.R
+++ tests/testthat/helper-taxon_extractor.R
@@ -1,2 +1,2 @@
 testing_db <- FishLife::FishBase_and_Morphometrics
-testing_sampling_data <- readRDS(test_path('test_data', 'mul_species_details.rds'))
+testing_sample_data <- readRDS(test_path('test_data', 'mul_species_details.rds'))
