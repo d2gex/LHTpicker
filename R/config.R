db125b53bb7aa0741939ccc13e4764207eb98f32
d2gex <daniel.garcia@d2garcia.com>
2024-03-19 12:17:54 +0100

    Refactor names to cover all 8 LHT variables

--- R/config.R
+++ R/config.R
@@ -18,6 +18,7 @@ fishlife_context$lht_names <- list(
   M = "log.natural_mortality.",
   L50 = "log.length_maturity.",
   Amax = "log.age_max.",
+  Amat = "log.age_maturity.",
   Temperature = "temperature"
 )
 # Function conversion required for each parameter. See that the left hand side variables coincnide with
@@ -29,15 +30,19 @@ fishlife_context$transform_function <- list(
   log.natural_mortality. = log,
   log.length_maturity. = log,
   log.age_max. = log,
+  log.age_maturity. = log,
   temperature = base::identity
 )
 # User-defined LHT names to fishlife's nomencluture for output LHTs
 fishlife_context$lht_results <- list(
   Linf = "log(length_infinity)",
+  Winf = "log(weight_infinity)",
   K = "log(growth_coefficient)",
   M = "log(natural_mortality)",
   L50 = "log(length_maturity)",
-  Amax = "log(age_max)"
+  Amax = "log(age_max)",
+  Amat = "log(age_maturity)",
+  Temperature = "temperature"
 )
 fishlife_context$backtransform_function <- list(
   "log(length_infinity)" = exp,
@@ -45,5 +50,7 @@ fishlife_context$backtransform_function <- list(
   "log(growth_coefficient)" = exp,
   "log(natural_mortality)" = exp,
   "log(length_maturity)" = exp,
-  "log(age_max)" = exp
+  "log(age_max)" = exp,
+  "log(age_maturity)" = exp,
+  "temperature" = base::identity
 )
