c8b94df08049f6f478e22654d72ac27e6bcef06c
d2gex <daniel.garcia@d2garcia.com>
2024-03-22 11:40:12 +0100

    Add taxa_prediction_extractor unit test

---
+++ R/taxa_prediction_extractor.R
@@ -0,0 +1,70 @@
+#' Multiple Taxon Prediction Extractor class
+#'
+#' @description
+#' Class that extracts and transform predicted values of LHTs for multiple species obtained from FishLife
+TaxaPredictionExtractor <- R6::R6Class("TaxaPredictionExtractor", inherit = MixinUtilities, public = list(
+  update_prefix = NULL,
+  lht_names = NULL,
+  back_transform_matrix = NULL,
+  predicting_lht_df = NULL,
+  updated_lht_list = NULL,
+  # // @formatter:off
+  #' @description
+  #' Initialise the TaxaPredictionExtractor
+  #'
+  #' @param update_prefix string to be added as prefix to the column names keeping the obtained new LHT values
+  #' @param lht_names list of user-defined LHT names associated with their FishLife's counterparts
+  #' @param back_transform_matrix list of backward-transformation functions to be applied on obtained LHT from FishLife
+  #' @param predicting_lht_df dataframe keeping the original inputted LHTs per taxon
+  #' @param updated_lht_list list of updated LHTs per taxon obtained from Fishlife
+  #' @export
+  # // @formatter:on
+  initialize = function(update_prefix, lht_names, back_transform_matrix, predicting_lht_df, updated_lht_list) {
+    self$update_prefix <- update_prefix
+    self$lht_names <- lht_names
+    self$back_transform_matrix <- back_transform_matrix
+    self$predicting_lht_df <- predicting_lht_df
+    self$updated_lht_list <- updated_lht_list
+  },
+  # // @formatter:off
+  #' @description
+  #' Extract and backtransform all updated LHT values per taxon that have beed obtained from Fishlife
+  #'
+  #' @returns dataframe that keeps the original and updated LHT values per taxon
+  # // @formatter:on
+  extract_and_backtransform = function() {
+    results <- self$predicting_lht_df
+    for (ind_species in names(self$updated_lht_list)) {
+      predicted_taxon_lht <- (self$updated_lht_list[[ind_species]])$updatemean_j
+      tax_extractor <- TaxonPredictionExtractor$new(self$update_prefix,
+                                                    self$lht_names,
+                                                    self$back_transform_matrix,
+                                                    predicted_taxon_lht)
+      taxon_details <- tax_extractor$extract_and_backtransform()
+      results <- private$update_lht_results(results, taxon_details, ind_species)
+    }
+    return(results)
+  }
+), private = list(
+  # // @formatter:off
+  #' @description
+  #' Update a dataframe that already holds the original LHT values with those updated for each taxon
+  #'
+  #' @returns updated dataframe of results with both the original and updated LHT values
+  # // @formatter:on
+  update_lht_results = function(results, taxon_details, taxon_name) {
+    updated_lht_names <- names(taxon_details)
+    result_columns <- names(results)
+
+    # Add updated columns to results df if it is the first time updated LHT values are to be added
+    if (!length(intersect(result_columns, updated_lht_names))) {
+      updated_empty_df <- self$create_empty_dataframe(updated_lht_names)
+      updated_empty_df[nrow(results),] <- NA
+      results <- cbind(results, updated_empty_df)
+    }
+    for (lht_name in updated_lht_names) {
+      results[results$species == taxon_name, lht_name] <- taxon_details[, lht_name]
+    }
+    return(results)
+  }
+))
---
+++ tests/testthat/test-taxa_prediction_extractor.R
@@ -0,0 +1,27 @@
+test_that("Multiple taxon's LHTS have been extracted and backtransformed", {
+  p_lht_gen <- UpdatedLHTGenerator$new(
+    testing_db,
+    testing_sample_data,
+    fishlife_context$transform_function,
+    fishlife_context$lht_names
+  )
+  updated_lhts <- p_lht_gen$update()
+
+  taxa_extractor <- TaxaPredictionExtractor$new(
+    fishlife_context$updated_prefix,
+    fishlife_context$lht_names,
+    fishlife_context$backtransform_function,
+    testing_sample_data,
+    updated_lhts
+  )
+  all_lhts_df <- taxa_extractor$extract_and_backtransform()
+  non_logarithmic_scale <- 10
+
+  expect_true(nrow(all_lhts_df) == 2)
+  expect_true(any(!is.na(all_lhts_df[all_lhts_df$species == 'Trisopterus luscus',])))
+  expect_true(any(!is.na(all_lhts_df[all_lhts_df$species == 'Pollachius pollachius',])))
+
+  # Ensure values have been backtransformed
+  expect_true(all_lhts_df[all_lhts_df$species == 'Trisopterus luscus', 'Linf'] > non_logarithmic_scale)
+  expect_true(all_lhts_df[all_lhts_df$species == 'Pollachius pollachius', 'Linf'] > non_logarithmic_scale)
+})
