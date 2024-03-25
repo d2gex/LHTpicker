e107ad4def2c661bc7a15b11478d616554f233c7
d2gex <daniel.garcia@d2garcia.com>
2024-03-25 18:04:02 +0100

    Refactor Fishlife Context class to take default parameters

--- R/config.R
+++ R/config.R
@@ -15,38 +15,46 @@ FishlifeLHTNameSpace <- R6::R6Class("FishlifeLHTNameSpace", public = list(
   # // @formatter:off
   #' @field backtransform_function Backward function conversion required for each obtained LHT value.
   # // @formatter:on
-  backtransform_function = NULL
-
+  backtransform_function = NULL,
+    #' @description
+    #' Initialise Fishlife Context class
+    #'
+    #' @export
+  initialize = function(updated_prefix = 'updated',
+                        lht_names = list(
+                          Linf = "log(length_infinity)",
+                          Winf = "log(weight_infinity)",
+                          K = "log(growth_coefficient)",
+                          M = "log(natural_mortality)",
+                          L50 = "log(length_maturity)",
+                          Amax = "log(age_max)",
+                          Amat = "log(age_maturity)",
+                          Temperature = "temperature"
+                        ),
+                        transform_function = list(
+                          "log(length_infinity)" = log,
+                          "log(weight_infinity)" = log,
+                          "log(growth_coefficient)" = log,
+                          "log(natural_mortality)" = log,
+                          "log(length_maturity)" = log,
+                          "log(age_max)" = log,
+                          "log(age_maturity)" = log,
+                          "temperature" = base::identity
+                        ),
+                        backtransform_function = list(
+                          "log(length_infinity)" = exp,
+                          "log(weight_infinity)" = exp,
+                          "log(growth_coefficient)" = exp,
+                          "log(natural_mortality)" = exp,
+                          "log(length_maturity)" = exp,
+                          "log(age_max)" = exp,
+                          "log(age_maturity)" = exp,
+                          "temperature" = base::identity
+                        )) {
+    self$updated_prefix <- updated_prefix
+    self$lht_names <- lht_names
+    self$transform_function <- transform_function
+    self$backtransform_function <- backtransform_function
+  }
 ))
 fishlife_context <- FishlifeLHTNameSpace$new()
-fishlife_context$updated_prefix <- 'updated'
-fishlife_context$lht_names <- list(
-  Linf = "log(length_infinity)",
-  Winf = "log(weight_infinity)",
-  K = "log(growth_coefficient)",
-  M = "log(natural_mortality)",
-  L50 = "log(length_maturity)",
-  Amax = "log(age_max)",
-  Amat = "log(age_maturity)",
-  Temperature = "temperature"
-)
-fishlife_context$transform_function <- list(
-  "log(length_infinity)" = log,
-  "log(weight_infinity)" = log,
-  "log(growth_coefficient)" = log,
-  "log(natural_mortality)" = log,
-  "log(length_maturity)" = log,
-  "log(age_max)" = log,
-  "log(age_maturity)" = log,
-  "temperature" = base::identity
-)
-fishlife_context$backtransform_function <- list(
-  "log(length_infinity)" = exp,
-  "log(weight_infinity)" = exp,
-  "log(growth_coefficient)" = exp,
-  "log(natural_mortality)" = exp,
-  "log(length_maturity)" = exp,
-  "log(age_max)" = exp,
-  "log(age_maturity)" = exp,
-  "temperature" = base::identity
-)
